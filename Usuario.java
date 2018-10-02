547a948588aec5b7e8c671a3abaa5b380ecca41e
JulioFigueiredo <37143216+JulioFigueiredo@users.noreply.github.com>
2018-10-02 08:33:33 -0300

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,25 @@
+public class Usuario {
+  private String apelido;
+  private String senha;
+
+ public Usuario(){
+   this.apelido = "null";
+   this.senha = 123;
+ } 
+ public Usuario(String nome, String senha){
+   
+ }
+ public String getApelido(){
+   return this.apelido;
+ }
+ public void setApelido(String apelido){
+   this.apelido = apelido;
+ }
+}
+ public String getSenha(){
+   return this.senha;
+ }
+ public void setSenha(String senha){
+   this.senha = senha;
+   
+ }
