901b8ba6d8e29e5aea7a32a88b69cb3a9f5ff98a
JulioFigueiredo <37143216+JulioFigueiredo@users.noreply.github.com>
2018-10-02 12:47:38 -0300

    Create Laboratorio.java

---
+++ Laboratorio.java
@@ -0,0 +1,36 @@
+
+package projeto;
+
+public class Laboratorio {
+    private String descricao;
+    private int capacidade;
+    
+    public Laboratorio() {
+        this.descricao = null;
+        this.capacidade = 0;        
+    }
+    public Laboratorio(String desc, int capac){
+        this.capacidade = capac;
+        this.descricao = desc;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCapacidade() {
+        return this.capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+    void Visualizar(){
+        System.out.println("Descrição: "+this.descricao);
+        System.out.println("capacidade: "+this.capacidade);
+    }
+}
