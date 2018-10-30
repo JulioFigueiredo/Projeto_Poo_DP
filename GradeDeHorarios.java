0990c74081e5c3dfba4ff96223fb4a32cd744900
JulioFigueiredo <37143216+JulioFigueiredo@users.noreply.github.com>
2018-10-30 16:21:19 -0300

    Update GradeDeHorarios.java

--- GradeDeHorarios.java
+++ GradeDeHorarios.java
@@ -1,32 +1,22 @@
 package projeto;
 
-
-import java.util.ArrayList;
-
 public class GradeDeHorarios {
     int i;
     int c;
-    Horario[][] horarios = new Horario [6][5];
-    {    
-        horarios[0][1]=null;
-        horarios[0][2]=null;
-        horarios[0][3]=null;
-        horarios[0][4]=null;
-        horarios[0][5]=null;
-        
-        horarios[1][0]=null;
-        horarios[2][0]=null;
-        horarios[3][0]=null;
-        horarios[4][0]=null;
-        horarios[5][0]=null;
-        horarios[6][0]=null;
-    
-        for (i = 0; i < horarios.length; i++) {
-            for(c = 0; c < horarios.length; c++){
-                System.out.println(horarios[i][c] +" ");
+    Horario[][] horarios = new Horario [7][7];
+    @Override
+    public String toString(){    
+        String [] dias = {"segunda ","terça ","quarta ","quinta ","sexta"} ; 
+        String dados="";
+        for (i = 0; i <5 ; i++) {
+            dados+=dias[i] + "\n";
+            for(c = 0; c < 6 ; c++){
+                dados += horarios[i][c] +" ";
+            }
+            dados+="\n";
         }
-     }
-};
+        return dados;
+    }
 
     public void addHorarios(int dia, Horario h){
         this.horarios[h.getAula()][dia] = h;
