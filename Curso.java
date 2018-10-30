b234883740e9c8bf6b9f8315b7fe5791e452efcc
JulioFigueiredo <37143216+JulioFigueiredo@users.noreply.github.com>
2018-10-29 21:40:26 -0300

    Update Curso.java

--- Curso.java
+++ Curso.java
@@ -1,10 +1,16 @@
 
 package projeto;
 
+import java.util.ArrayList;
+
 public class Curso{
   private String descricao;
   private int codigo;
   private int duracao;
+  private ArrayList<Docente> professor;
+  private ArrayList<Turma> turma;
+  private ArrayList<Disciplina> disciplina;
+  
 
   public Curso(){
     this.descricao = null;
@@ -15,6 +21,9 @@ public class Curso{
    this.descricao = desc;
    this.codigo = codigo;
    this.duracao = duracao;
+   this.professor= new ArrayList<Docente>();
+   this.turma= new ArrayList<Turma>();
+   this.disciplina= new ArrayList<Disciplina>();
  }
  public String getDescricao(){
    return this.descricao;
@@ -34,11 +43,64 @@ public class Curso{
  public void setDuracao(int duracao){
    this.duracao = duracao;
  }
- void Visualizar(){
-     System.out.println("Descrição: "+this.descricao);
-     System.out.println("codigo: "+this.codigo);
-     System.out.println("Duração: "+this.duracao+ " anos");
-     
- }
+
+    public ArrayList<Docente> getProfessor() {
+        return this.professor;
+    }
+
+    public void setProfessor(ArrayList<Docente> professor) {
+        this.professor = professor;
+    }
+
+    public ArrayList<Turma> getTurma() {
+        return this.turma;
+    }
+
+    public void setTurma(ArrayList<Turma> turma) {
+        this.turma = turma;
+    }
+
+    public ArrayList<Disciplina> getDisciplina() {
+        return this.disciplina;
+    }
+
+    public void setDisciplina(ArrayList<Disciplina> disciplina) {
+        this.disciplina = disciplina;
+    }
+    public void addDocente(Docente d) {
+        this.professor.add(d);
+    }
+
+    public void removeDocente(int d) {
+        this.professor.remove(d);
+    }
+    
+    
+    public void addDisciplina(Disciplina disc) {
+        this.disciplina.add(disc);
+    }
+
+    public void removeDisciplina(int disc) {
+        this.disciplina.remove(disc);
+    }
+
+    public void addTurma(Turma t) {
+        this.turma.add(t);
+    }
+
+    public void removeTurma(int t) {
+        this.turma.remove(t);
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" + "descricao=" + descricao + ", codigo=" + codigo + ", duracao=" + duracao + ", professor=" + professor + ", turma=" + turma + ", disciplina=" + disciplina + '}';
+    }
+    
+    
+ 
+
+   
+
 }
 
